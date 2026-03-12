271501a0e2dd6bc6ebc424d93f30a4d5d63ad9f6
lakshay-bhatnagar <lakshaybhatnagar22@gmail.com>
2026-03-12 12:54:38 +0530

    fix:    fixed installation script bug

--- install.sh
+++ install.sh
@@ -73,7 +73,7 @@ go install github.com/tomnomnom/assetfinder@latest
 go install github.com/tomnomnom/waybackurls@latest
 go install github.com/lc/gau/v2/cmd/gau@latest
 go install github.com/sensepost/gowitness@latest
-go install github.com/projectdiscovery/ffuf@latest
+go install github.com/ffuf/ffuf/v2@latest
 
 echo "[+] Installing Arjun..."
 pip3 install arjun --break-system-packages >/dev/null 2>&1 || pip3 install arjun >/dev/null 2>&1
