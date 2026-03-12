cc06f78822f2e60237733d4df3c00cddddbd721a
lakshay-bhatnagar <lakshaybhatnagar22@gmail.com>
2026-03-12 19:15:22 +0530

    modified:   added aquatone installation

--- install.sh
+++ install.sh
@@ -74,6 +74,7 @@ go install github.com/tomnomnom/waybackurls@latest
 go install github.com/lc/gau/v2/cmd/gau@latest
 go install github.com/sensepost/gowitness@latest
 go install github.com/ffuf/ffuf/v2@latest
+o install github.com/michenriksen/aquatone@latest
 
 echo "[+] Installing Arjun..."
 pip3 install arjun --break-system-packages >/dev/null 2>&1 || pip3 install arjun >/dev/null 2>&1
